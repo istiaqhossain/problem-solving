f14310c22d7907723f8e285f1bfe9703160627dd
istiaqhossain <md.istiaqhossain1990@gmail.com>
2019-08-09 17:42:50 +0600

    uri-1051

--- URI/uri-1051.py
+++ URI/uri-1051.py
@@ -1,8 +1,21 @@
 def main():
     salary = float(input())
+    tax = 0
     if salary <= 2000.00:
         print('Isento')
     else:
         salary -= 2000.00
-        print(salary)
+        if salary > 1000.00:
+            salary -= 1000.00
+            tax += 1000*.08
+            if salary > 1500.00:
+                salary -= 1500.00
+                tax += 1500*.18
+                if salary > 00.00:
+                    tax += salary*.28
+            else:
+                tax += salary*.18
+        else:
+            tax += salary*.08
+        print('R$ %.2f'%tax)
 main()
